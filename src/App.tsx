6023d0f1194b8b27a83a5b0d4c3834f211c0764c
CadeWeiskopf <cweiskopf@thisiscsg.com>
2023-08-16 22:09:26 -0400

    experimenting with multi-hosting apps on k8s

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <img
+          src={logo}
+          className="App-logo"
+          alt="logo"
+        />
+        <p>k8s test 2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
